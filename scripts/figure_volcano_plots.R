526c613e19b2ee40b2b2fa9f9d908e28bd953bd6
Gibran Hemani <explodecomputer@gmail.com>
2018-05-09 11:29:05 +0100

    updated figs

--- scripts/figure_volcano_plots.R
+++ scripts/figure_volcano_plots.R
@@ -1,7 +1,7 @@
 library(TwoSampleMR)
 library(ggrepel)
 library(ggthemes)
-toggle_dev("test")
+# toggle_dev("test")
 
 ## Volcano plot of lipids on traits
 
@@ -42,24 +42,28 @@ fres$exposure[fres$exposure == "Lp(a)"] <- "Lipoprotein(a)"
 # fres <- subset(fres, ! outcome %in% c("Coronary heart disease", "Myocardial infarction") & !grepl("Intracranial volume", outcome))
 fres$sig <- fres$pval < 0.05
 fres$fdr <- p.adjust(fres$pval, method="fdr")
+fres$fdr2 <- "FDR < 5%"
+fres$fdr2[fres$fdr > 0.05] <- "FDR > 5%"
 
 fres <- subset(fres, exposure=="LDL cholesterol")
 
 ggplot(fres, aes(x=effect, y=-log10(pval))) +
 geom_vline(xintercept=0, linetype="dotted") +
 geom_point(data=subset(fres, !sig)) +
-geom_point(data=subset(fres, sig), aes(colour=category, size=fdr < 0.05)) +
+geom_point(data=subset(fres, sig), aes(colour=category, size=fdr2)) +
 # facet_grid(. ~ exposure, scale="free") +
-geom_label_repel(data=subset(fres, sig), aes(label=outcome, colour=category), segment.colour="black", point.padding = unit(0.7, "lines"), box.padding = unit(0.2, "lines"), segment.size=0.5, force=20, max.iter=3e3, alpha=0.9, size=3) +
+geom_label_repel(data=subset(fres, sig), aes(label=outcome, colour=category), segment.colour="black", point.padding = unit(0.7, "lines"), box.padding = unit(0.2, "lines"), segment.size=0.5, force=20, max.iter=3e3, alpha=0.9, size=3, show.legend=FALSE) +
 # geom_text_repel(data=subset(fres, sig), aes(label=outcome, colour=category)) +
 theme_bw() + 
 theme(panel.grid.major = element_blank(), 
       panel.grid.minor = element_blank(),
       strip.text.x=element_text(size=16)
 ) +
+scale_colour_brewer(type="qual", palette=2) +
 # scale_colour_brewer(type="qual", palette="Dark2") +
 # scale_fill_brewer(type="qual", palette="Dark2") +
-labs(x="Effect on outcome (SD or log(OR)) per SD lowering of lipids", size="5% FDR") +
+labs(x="Effect on outcome (SD or log(OR)) per SD lowering of lipids", size="") +
+scale_size_manual(values=c(4,1.5)) +
 xlim(c(-0.7, 0.7))
 ggsave("../images/volcano/volcano_boxes_ldl.png", width=8, height=8)
 
